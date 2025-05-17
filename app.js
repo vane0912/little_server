8ae75ac05961d9de2bd95d285866c56350954c1c
Vanessa Gonzalez <vanessagpro5@gmail.com>
2025-05-17 12:11:57 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000 
+
+var Orders 
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send(Orders)
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  Orders = {
+    MIN : req.body.MIN,
+    Rejected: req.body.Rejected,
+    Completed: req.body.Completed
+  }
+  res.send({send: 'Got post request'})
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
