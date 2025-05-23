81a82fefe0376c76126ee3ba72b5088adcef3275
Vanessa Gonzalez <vanessagpro5@gmail.com>
2025-05-23 16:01:46 -0600

    add cancelled

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 3000
 var Orders = {
     Min: "",
     Rejected: "",
-    Completed: ""
+    Completed: "",
+    Cancelled: ""
 }
 app.use(express.json())
 app.use(cors())
@@ -20,6 +21,7 @@ app.post('/', (req, res) => {
   console.log(req.body)
   req.body.MIN ? Orders.Min = req.body.MIN :
   req.body.Rejected ? Orders.Rejected = req.body.Rejected :
+  req.body.Cancelled ? Orders.Cancelled = req.body.Cancelled :
   Orders.Completed = req.body.Completed
 
   res.send({send: 'Got post request'})
