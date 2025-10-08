cef10094c3762cc2e6679c0252e3bbcf59846989
Vanessa Gonzalez <vanessagpro5@gmail.com>
2025-10-08 10:36:46 -0600

    add translations endpoint

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
-var cors = require('cors')
+let cors = require('cors')
+const { send_message } = require('./message_slack')
 const app = express()
 const port = process.env.PORT || 3000 
 
 
-var Orders = {
+let Orders = {
     Min: "",
     Rejected: "",
     Completed: "",
@@ -19,15 +20,21 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  console.log(req.body)
   req.body.MIN ? Orders.Min = req.body.MIN :
   req.body.Rejected ? Orders.Rejected = req.body.Rejected :
   req.body.Cancelled ? Orders.Cancelled = req.body.Cancelled :
   req.body.Scheduling ? Orders.Scheduling = req.body.Scheduling :
   Orders.Completed = req.body.Completed
-
   res.send({send: 'Got post request'})
+})
 
+app.post("/translations", async (req, res) => {
+  try{
+    await send_message(req.body)
+    res.send({message: "Message sent"})
+  }catch{
+    res.send({message: "Error Ocurred"})
+  }
 })
 
 app.listen(port, () => {
